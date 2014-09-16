f79976f668b4d9fa68e1b3878b26a0efd75ab443
misclinux <miscuser@gmail.com>
2014-09-16 17:44:17 -0500

    readding

---
+++ nightly_backup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Bash script to back up files
+# Runs nightly around 3am
+
+log=~/logs/nightly_backup_log.txt
+
+date >> $log
+echo "Backup starting..." >> $log
+
+##### Files stored on computer
+
+# Documents folder 
+echo "Finance files..." >> $log
+rsync -avhO --log-file="$log" ~/Documents/ /media/backup/Documents/ #--dry-run
+
+# Language files 
+echo "Language files..." >> $log
+rsync -avhO --log-file="$log" ~/Language/ /media/backup/language/ #--dry-run
+
+# Stacey's files 
+echo "Stacey's files..." >> $log
+rsync -avhO --log-file="$log" ~/Stacey\_documents/ /media/backup/Stacey\_documents/ #--dry-run
+
+
+
+##### Files stored on external drive
+
+# Music -- ~/Music is a symlink to the external drive
+echo "Music files..." >> $log
+rsync -avhO --log-file="$log" ~/Music/ /media/backup/Music/ #--dry-run
+
+# Video files -- ~/Videos is a symlink to the exteranl drive
+echo "Video files..." >> $log
+rsync -avhO --log-file="$log" ~/Videos/ /media/backup/Videos/ #--dry-run
+
+# Pictures
+echo "Pictures..." >> $log
+rsync -avhO --log-file="$log" /media/external/pictures/ /media/backup/pictures/ #--dry-run
+
+# Subversion Repository
+# echo "Subversion repository..." >> $log
+# rsync -avhO --log-file="$log" /cygdrive/c/svn_repository/ /cygdrive/i/svn_repository/ #--dry-run
+ 
+# Chessbase files
+# echo "ChessBase files..." >> $log
+# rsync -avhO --log-file="$log" /cygdrive/c/Users/main/Documents/ChessBase/ /cygdrive/i/Chess/Chessbase/ #--dry-run
+ 
+# Chess - other
+# echo "Chess - other..." >> $log
+# rsync -avhO --log-file="$log" /cygdrive/c/Chess/ /cygdrive/i/Chess/other/ #--dry-run
+ 
+echo "Backup complete." >> $log
+
