cbfdaf2516e5d2663c6b567ca26e88338b2972c4
misclinux <miscuser@gmail.com>
2014-11-20 23:10:21 -0600

    Excluded !Missed videos

--- nightly_backup.sh
+++ nightly_backup.sh
@@ -42,7 +42,7 @@ rsync -avhO --log-file="$log" ~/Music/ /media/backup/media/Music/ #--dry-run
 
 # Video files -- ~/Videos is a symlink to the exteranl drive
 echo "Video files..." >> $log
-rsync -avhO --log-file="$log" ~/Videos/ /media/backup/media/Videos/ #--dry-run
+rsync -avhO --exclude '!Missed' --log-file="$log" ~/Videos/ /media/backup/media/Videos/ #--dry-run
 
 # Pictures
 echo "Pictures..." >> $log
