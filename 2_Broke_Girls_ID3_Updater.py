8166b28a72842cbaaec5d42ba587112175cf6abb
misclinux <miscuser@gmail.com>
2015-11-09 18:57:05 -0600

    Updated formatting. Make flake8 compliant.

--- 2_Broke_Girls_ID3_Updater.py
+++ 2_Broke_Girls_ID3_Updater.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python2.7
-# Script to automatically rename episodes of 2 Broke Girls
+# Script to save ID3 data to my 2 Broke Girls mp3s.
 #   The album needs to be updated each season
 #   The artwork can also be updated each season
-# Use: Drag the file
 
-import os, sys, string, re
+import sys
+import re
 import eyed3
+
 
 def getEpisode(filename):
     try:
         episode = re.search('x(.+?)\ ', filename).group(1)
     except AttributeError:
-        episode = 'not found' # apply your error handling
+        episode = 'not found'
     return episode
+
 
 def getTitle(filename):
     t = filename.split('-')        # do this the simple way since I know the naming is correct
@@ -23,20 +25,20 @@ def getTitle(filename):
         pass
     return s
 
-    
-def update_id3(mp3_file_name, artwork_file_name, artist, album, genre, item_title, trackno):    
+
+def update_id3(mp3_file_name, artwork_file_name, artist, album, genre, item_title, trackno):
     audiofile = eyed3.load(mp3_file_name)
 
     if audiofile.tag is None:                           # needed if frame does not exist
         audiofile.tag = eyed3.id3.Tag()
         audiofile.tag.file_info = eyed3.id3.FileInfo(mp3_file_name)
 
-    imagedata = open(artwork_file_name,'rb').read()
-    audiofile.tag.images.set(3,imagedata,"image/jpeg",u"")  # a description can go in the last part
+    imagedata = open(artwork_file_name, 'rb').read()
+    audiofile.tag.images.set(3, imagedata, "image/jpeg", u"")  # a description can go in the last part
     audiofile.tag.artist = artist                       # artist
     audiofile.tag.album = album                         # album title
     audiofile.tag.title = item_title.decode('utf-8')    # unicode
-    audiofile.tag.track_num = (trackno,"")              # tuple of (track, total tracks)
+    audiofile.tag.track_num = (trackno, "")              # tuple of (track, total tracks)
     audiofile.tag.genre = genre                         # genre
     audiofile.tag.save()
 
@@ -49,9 +51,8 @@ def main():
     tracknumber = getEpisode(mp3_file_name)
     item_title = getTitle(mp3_file_name)
 
-    update_id3(mp3_file_name, artwork_file_name, artist, album, genre, item_title, tracknumber)    
+    update_id3(mp3_file_name, artwork_file_name, artist, album, genre, item_title, tracknumber)
 
 
 if __name__ == '__main__':
     main()
-
