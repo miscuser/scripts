a42c04cdb1b820ee54e92d6b0d7f075882b91b74
misclinux <miscuser@gmail.com>
2016-08-05 15:48:07 -0500

    Initial commit

---
+++ install-scripts/osx/setup-osx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Configure OSX.
+
+# Clone other repos.
+# ------------------
+git clone git://github.com/miscuser/dotfiles.git ~/dotfiles
+git clone git://github.com/miscuser/bin.git ~/bin
+git clone git://github.com/miscuser/colors.git ~/.vim/colors
+
+# Alias dotfiles in home directory.
+# ---------------------------------
+sudo ~/scripts/install-scripts/osx/create-symlinks.sh
+
+# Install useful packages.
+brew install tmux
+brew install the_silver_searcher
+brew install ack
+brew install vim
