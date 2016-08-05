8c32b2565bbe4b4a6435720aa3b745c4f2457249
misclinux <miscuser@gmail.com>
2016-08-05 15:42:04 -0500

    Initial commit

---
+++ install-scripts/osx/create-symlinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install symlinks to dotfiles.
+dotfiles_dir=~/dotfiles
+
+# Delete existing dot files.
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.bashrc > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -rf ~/.agignore > /dev/null 2>&1
+
+# Create symlinks in home folder.
+#==============
+ln -sf $dotfiles_dir/.vimrc ~/.vimrc
+ln -sf $dotfiles_dir/.bashrc ~/.bashrc
+ln -sf $dotfiles_dir/.tmux-conf ~/.tmux-conf
+ln -sf $dotfiles_dir/.agignore ~/.agignore
