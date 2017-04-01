4f980a4967f32191c52d48f01c6e2372e7e59a97
Misc User <miscuser@gmail.com>
2017-04-01 18:58:43 -0500

    Changed echo to printf

--- backup-scripts/nightly_backup_windows.sh
+++ backup-scripts/nightly_backup_windows.sh
@@ -7,12 +7,12 @@ log='/cygdrive/c/Dropbox/logs/nightly/nightly_backup_windows.txt'
 backupdrive='/cygdrive/i/'
 
 date >> $log
-echo "Backup starting..." | tee -a $log
+printf "Backup starting...\n" | tee -a $log
 
 ##### Files stored on computer
 
 # Home folder 
-echo "\n--- Windows home folder\n" >> $log
+printf "\n--- Windows home folder\n" >> $log
 rsync -avhO --log-file="$log" /cygdrive/c/home/ $backupdrive/home/ #--dry-run
 
 # The woman's files. 
@@ -26,16 +26,16 @@ rsync -avhO --log-file="$log" /cygdrive/c/home/ $backupdrive/home/ #--dry-run
 ##### Files stored on external drive
 
 # Music -- ~/Music is a symlink to the external drive
-echo "\n--- Music files\n" | tee -a $log
-rsync -avhO --exclude-from='/home/main/scripts/backup-scripts/exclude_music.txt' --log-file="$log" ~/Music/ $backupdrive/media/Music/ #--dry-run
+printf "\n--- Music files\n" | tee -a $log
+rsync -avhO --exclude-from='/home/main/scripts/backup-scripts/exclude_music.txt' --log-file="$log" ~/Music/ $backupdrive/media/Music/  | grep -E -v '/$' #--dry-run
 
 # Video files -- ~/Videos is a symlink to the exteranl drive
-echo "\n--- Video files\n" | tee -a $log
-rsync -avhO --exclude-from='/home/main/scripts/backup-scripts/exclude_video.txt' --log-file="$log" ~/Videos/ $backupdrive/media/Videos/ #--dry-run
+printf "\n--- Video files\n" | tee -a $log
+rsync -avhO --exclude-from='/home/main/scripts/backup-scripts/exclude_video.txt' --log-file="$log" ~/Videos/ $backupdrive/media/Videos/  | grep -E -v '/$' #--dry-run
 
 # Pictures
 # echo "Pictures..." >> $log
 # rsync -avhO --log-file="$log" /media/external/pictures/ /media/backup/media/pictures/ #--dry-run
 
-echo "\nBackup complete." | tee -a $log
-echo "\n------------------------------------" | tee -a $log
+printf "\nBackup complete." | tee -a $log
+printf "\n------------------------------------" | tee -a $log
