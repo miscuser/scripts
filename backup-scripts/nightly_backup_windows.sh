9848fcc077b1b0cfd5c773bc5a317a9b4c79f024
misc <miscuser@gmail.com>
2017-04-21 13:23:11 -0500

    Simplified again

--- backup-scripts/nightly_backup_windows.sh
+++ backup-scripts/nightly_backup_windows.sh
@@ -3,34 +3,31 @@
 # Backup from Windows machine. 
 
 log='/cygdrive/c/Dropbox/logs/nightly/nightly_backup_windows.txt'
-localhome='/cygdrive/c/home/'
-backupdrive='/cygdrive/i/'
-mediadrive='/cygdrive/j/'
+backup_drive='/cygdrive/i/'
+media_drive='/cygdrive/j/'
+c_drive='/cygdrive/c'
 
 date >> $log
 printf "Backup starting...\n" | tee -a $log
 
-##### Files stored on computer.
-printf "\n--- Windows home folder\n" | tee -a $log
-rsync -avhO --log-file="$log" $localhome $backupdrive/win/home/ | grep -E -v '/$' #--dry-run
-
-printf "\n--- Portable apps\n" | tee -a $log
-rsync -avhO --log-file="$log" /cygdrive/c/apps/ $backupdrive/win/apps | grep -E -v '/$' #--dry-run
-
-printf "\n--- Installation packages\n" | tee -a $log
-rsync -avhO --log-file="$log" /cygdrive/c/installs/ $backupdrive/win/installs | grep -E -v '/$' #--dry-run
-
-printf "\n--- Windows bin folder\n" | tee -a $log
-rsync -avhO --log-file="$log" /cygdrive/c/bin/ $backupdrive/win/bin | grep -E -v '/$' #--dry-run
+# Misc. Windows folders.
+ rsync -av --log-file="$log" \
+     $c_drive/home \
+     $c_drive/apps \
+     $c_drive/installs \
+     $c_drive/bin \
+     /cygdrive/i/win \
+     | grep -E -v '/$'
 
 ##### Media files stored on external drive.
 printf "\n--- External (media) files\n" | tee -a $log
-rsync -avhO \
+rsync -av \
     --exclude-from='/home/main/scripts/backup-scripts/exclude_video.txt' \
     --exclude-from='/home/main/scripts/backup-scripts/exclude_music.txt' \
     --exclude-from='/home/main/scripts/backup-scripts/exclude_junk.txt' \
     --log-file="$log" \
-    $mediadrive $backupdrive/media/ | grep -E -v '/$' #--dry-run
+    $media_drive $backup_drive/media/ \
+    | grep -E -v '/$' 
 
 printf "\nBackup complete." | tee -a $log
 printf "\n------------------------------------" | tee -a $log
