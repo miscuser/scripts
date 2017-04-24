61224c0ae473f8ec35e41ce714eaf09048780c54
misc <miscuser@gmail.com>
2017-04-24 13:07:13 -0500

    Initial commit

---
+++ backup-scripts/offsite_backup_windows.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Backup from BACKUP to OFFSITE drive. 
+
+# Command lines with and without the dry-run option.
+cmd='rsync -avh'
+cmd='rsync -avh --dry-run'
+
+# Reference folders and files.
+backup_drive='/cygdrive/i/'
+offsite_drive='/cygdrive/z/'
+
+printf "\n--- Copying from BACKUP to OFFSITE\n"
+eval $cmd \
+     $backup_drive \
+     $offsite_drive \
+     | grep -E -v '/$'
