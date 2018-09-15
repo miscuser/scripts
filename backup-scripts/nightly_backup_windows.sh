acf3f2c9977ddadc3b9454a4ae3af99e9c6c298b
Fred <gripper555@gmail.com>
2018-09-15 10:23:33 -0500

    Removed extra detail in logs.

--- backup-scripts/nightly_backup_windows.sh
+++ backup-scripts/nightly_backup_windows.sh
@@ -7,17 +7,15 @@ cmd='rsync -avh --log-file="$log"'
 # cmd='rsync -avh --log-file="$log" --dry-run'
 
 # Reference folders and files.
-log='/cygdrive/c/Dropbox/logs/nightly/nightly_backup_windows.txt'
+log='/cygdrive/c/Users/main/Google Drive/logs/nightly/nightly_backup_windows.txt'
 backup_drive='/cygdrive/i/'
 media_drive='/cygdrive/z/'
 c_drive='/cygdrive/c'
 
 # Logging info. 
 date >> $log
-printf "Backup starting...\n" | tee -a $log
 
-# Misc. Windows folders.
-printf "\n--- Various Windows folders\n" | tee -a $log
+# Misc. local folders.
 eval $cmd \
     --exclude-from='/home/main/scripts/backup-scripts/exclude_junk.txt' \
      $c_drive/home \
@@ -31,14 +29,10 @@ eval $cmd \
      /cygdrive/i/win \
      | grep -E -v '/$'
 
-# Media files stored on external drive.
-printf "\n--- External (media) files\n" | tee -a $log
+# Media files.
 eval $cmd \
     --exclude-from='/home/main/scripts/backup-scripts/exclude_video.txt' \
     --exclude-from='/home/main/scripts/backup-scripts/exclude_music.txt' \
     --exclude-from='/home/main/scripts/backup-scripts/exclude_junk.txt' \
     $media_drive $backup_drive/media/ \
     | grep -E -v '/$'
-
-printf "\nBackup complete." | tee -a $log
-printf "\n------------------------------------" | tee -a $log
