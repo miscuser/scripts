5c538e40bec3b17b646affb9446c940d9063f57f
Fred <gripper555@gmail.com>
2018-10-06 12:57:22 -0500

    Now only doing media

--- backup-scripts/nightly_backup_windows.sh
+++ backup-scripts/nightly_backup_windows.sh
@@ -16,18 +16,12 @@ c_drive='/cygdrive/c'
 date >> $log
 
 # Misc. local folders.
-eval $cmd \
-    --exclude-from='/home/main/scripts/backup-scripts/exclude_junk.txt' \
-     $c_drive/home \
-     $c_drive/apps \
-     $c_drive/installs \
-     $c_drive/bin \
-     $c_drive/static \
-     $c_drive/shortcuts \
-     $c_drive/Dropbox \
-     $c_drive/users/main/'Google\ Drive' \
-     /cygdrive/i/win \
-     | grep -E -v '/$'
+# eval $cmd \
+    # --exclude-from='/home/main/scripts/backup-scripts/exclude_junk.txt' \
+     # $c_drive/bin \
+     # $c_drive/static \
+     # /cygdrive/i/win \
+     # | grep -E -v '/$'
 
 # Media files.
 eval $cmd \
