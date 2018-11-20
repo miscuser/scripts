b6b6c96e1fa69e612bd1ee783b6954dc3432e4e4
Fred <gripper555@gmail.com>
2018-11-19 22:20:18 -0600

    Removed junk

--- backup-scripts/nightly_backup_windows.sh
+++ backup-scripts/nightly_backup_windows.sh
@@ -15,14 +15,6 @@ c_drive='/cygdrive/c'
 # Logging info. 
 date >> $log
 
-# Misc. local folders.
-# eval $cmd \
-    # --exclude-from='/home/main/scripts/backup-scripts/exclude_junk.txt' \
-     # $c_drive/bin \
-     # $c_drive/static \
-     # /cygdrive/i/win \
-     # | grep -E -v '/$'
-
 # Media files.
 eval $cmd \
     --exclude-from='/home/main/scripts/backup-scripts/exclude_video.txt' \
